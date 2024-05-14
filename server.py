79990f572876adc7c51aa4659bbe9c977d46385f
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-14 10:53:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.example.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@example.com'
+app.config['MAIL_USERNAME'] = 'vibudesh0407@gmail.com'
 app.config['MAIL_PASSWORD'] = 'your_password'
 app.config['MAIL_DEFAULT_SENDER'] = 'your_email@example.com'
 
